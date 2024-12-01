620a9b259b9216040d4a8133e0e872a14dc2899c
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-12-01 22:04:26 +0900

    fix : MapPage일 때 Header 숨김처리

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 
 const Layout = () => {
+  const { pathname } = useLocation();
+  const isMapPage = pathname === '/map';
+
+  if (isMapPage) return <Outlet />;
+
   return (
     <>
       <Header />
