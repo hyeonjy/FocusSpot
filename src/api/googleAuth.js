f4b991a4aa64beba72f4f18bc52328ba5d90fe29
sanha <heftycornerstone@gmail.com>
2024-12-02 12:03:50 +0900

    Fix: redirection 주소 수정함

--- src/api/googleAuth.js
+++ src/api/googleAuth.js
@@ -1,6 +1,10 @@
 import supabase from "./superbaseClient"
 
-const googleSignIn = async () => {
+//지금은 각자 개발중에 사용하고 있는 로컬호스트 url의 포트 번호를 마저 기입해서 사용해주세요
+//vercel 배포하고 난 다음에 경로를 제대로 재설정해줍시다!
+const hostingUrl = 'http://localhost:8088'
+
+const googleSignIn = async (redirectTo) => {
     const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
@@ -8,6 +12,7 @@ const googleSignIn = async () => {
                 access_type: 'offline',
                 prompt: 'consent',
             },
+            redirectTo: `${hostingUrl}/${redirectTo}`
         },
     });
     if(error) console.error(error);
@@ -24,6 +29,9 @@ export const getUserAuth = async () => {
 }
 
 export const signIn = async () => {
-    await googleSignIn();
+    //이미 회원가입한 유저는 홈으로, 그렇지 않은 유저는 로그인 페이지에서 마저 회원정보를 등록하도록
+    const isMember = true;
+    const redirectTo = (isMember) ? '' :'login' 
+    await googleSignIn(redirectTo);
     await getUserAuth();
 }
