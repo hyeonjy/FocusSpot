def1f3f115d76822b9f4a427ba588c0c3896fb4c
sanha <heftycornerstone@gmail.com>
2024-12-02 20:47:37 +0900

    chore: persist로 저장하는 유저 상태 key값 userAuth로 설정

--- src/zustand/userStore.js
+++ src/zustand/userStore.js
@@ -6,4 +6,19 @@ export const useUserStore = create(persist(set => ({
     isAuthenticated: false,
     setIsAuthenticated: (boolean) => set(() => ({ isAuthenticated: boolean })),
     setIsGonnaSignOut: (boolean) => set(() => ({ isGonnaSignOut: boolean }))
-})))
+}), {
+    name: "userAuth",
+}
+))
+
+/*
+create(
+  persist(
+    {
+      name: "todos-storage", // 저장소 이름을 설정해요!
+      // getStorage: () => sessionStorage, // localStorage가 아닌 곳에 저정하고 싶다면!
+    }
+  )
+);
+
+*/
