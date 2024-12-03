1fb230ed5b6b90941ee99bab5f2937127f14041e
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2024-12-03 20:09:17 +0900

    feat: 임시 사용자 로그인 여부 변경

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,16 +1,16 @@
-import { useEffect, useState } from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = () => {
   // const { isAuthenticated } = useUserStore();
   // 임시 사용자 로그인 여부
   let isAuthenticated = false;
 
-  const isToken = window.localStorage.getItem("sb-uehotuivypwxyqiejuvc-auth-token");
+  const isToken = window.localStorage.getItem('sb-uehotuivypwxyqiejuvc-auth-token');
   if (isToken) isAuthenticated = true;
 
   //const isAuthenticated = true;
-  return isAuthenticated ? <Outlet /> : <Navigate to={"/login"} replace />;
+  return isAuthenticated ? <Outlet /> : <Navigate to={'/login'} replace />;
 };
 
 export default ProtectedRoute;
