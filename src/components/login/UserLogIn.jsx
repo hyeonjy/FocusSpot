4737335d6f38e6494dfb69d306a4381d201e92e7
sanha <heftycornerstone@gmail.com>
2024-12-04 20:29:27 +0900

    chore: 불필요한 코드 삭제

--- src/components/login/UserLogIn.jsx
+++ src/components/login/UserLogIn.jsx
@@ -1,66 +1,58 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { useUserStore } from "../../zustand/userStore";
-import { googleSignIn, googleSignOut } from "../../api/googleAuth";
+import { googleSignIn } from "../../api/googleAuth";
 import { useEffect, useState } from "react";
 import google_icon from "../../../public/google_icon.svg"
 import styled from "styled-components";
 import Spinner from "../Spinner";
 
 export const UserLogIn = ({ isLoading }) => {
-    const location = useLocation();
-    const navigate = useNavigate();
-    const [token, setToken] = useState(null);
-    const { setIsAuthenticated, id, setId, setName, setEmail, setProfileImg } = useUserStore();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [token, setToken] = useState(null);
+  const { setIsAuthenticated, id } = useUserStore();
 
-    const handleSignin = async () => {
-        try{
-            await googleSignIn();
-        } catch(error) {
-            console.error(`소셜 로그인 실패 Error: ${error}`);
-            window.alert('소셜 로그인에 실패했습니다. 다시 시도해주세요.');
-        }
-    };
-    const handleSignOut = () => {
-        setId(null)
-        setName(null)
-        setEmail(null)
-        setProfileImg(null);
+  const handleSignin = async () => {
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.error(`소셜 로그인 실패 Error: ${error}`);
+      window.alert('소셜 로그인에 실패했습니다. 다시 시도해주세요.');
+    }
+  };
 
-        googleSignOut();
-    };
+  useEffect(() => {
+    const hash = location.hash;
+    const params = new URLSearchParams(hash.replace('#', ''));
+    const userToken = params.get('access_token');
 
-    useEffect(() => {
-        const hash = location.hash;
-        const params = new URLSearchParams(hash.replace('#', ''));
-        const userToken = params.get('access_token');
+    if (userToken) setToken(true);
+    if (token && id) {
+      window.alert('로그인에 성공했습니다. 홈으로 갑니다.');
+      navigate('/');
+    }
 
-        if (userToken) setToken(true);
-        if (token && id) {
-            window.alert('로그인에 성공했습니다. 홈으로 갑니다.');
-            navigate('/');
-        }
-        
-        return () => {
-            if (token && id) setIsAuthenticated(true);
-        }
-    }, [id]);
+    return () => {
+      if (token && id) setIsAuthenticated(true);
+    }
+  }, [id]);
 
-    return (
-      <>
-        {isLoading ? (
-          <Spinner/>
-        ) : (
-          <>
-            <StH2>로그인</StH2>
-            <StSocialLoginBtn onClick={handleSignin}>
-              <img src={google_icon} alt="signin_button_icon" />
-              <span>Google 계정으로 시작</span>
-            </StSocialLoginBtn>
-            <StRefer>Google 계정으로 시작할 수 있습니다</StRefer>
-          </>
-        )}
-      </>
-    );
+  return (
+    <>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <StH2>로그인</StH2>
+          <StSocialLoginBtn onClick={handleSignin}>
+            <img src={google_icon} alt="signin_button_icon" />
+            <span>Google 계정으로 시작</span>
+          </StSocialLoginBtn>
+          <StRefer>Google 계정으로 시작할 수 있습니다</StRefer>
+        </>
+      )}
+    </>
+  );
 };
 
 const StH2 = styled.h2`
