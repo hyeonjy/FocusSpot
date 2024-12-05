7dd9a0f4a383bea7919787de3db2a9b7813f34e6
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2024-12-05 09:42:27 +0900

    chore: url 경로변경

--- src/api/googleAuth.js
+++ src/api/googleAuth.js
@@ -1,51 +1,45 @@
-import supabase from "./superbaseClient"
+import supabase from './superbaseClient';
 
-//vercel 배포하고 난 다음에 경로를 제대로 재설정해줍시다!
-const hostingUrl = 'http://localhost:8088'
+const HOSTING_URL = 'https://focus-spot.vercel.app';
 
 export const googleSignIn = async () => {
-    const { data, error } = await supabase.auth.signInWithOAuth({
-        provider: 'google',
-        options: {
-            queryParams: {
-                access_type: 'offline',
-                prompt: 'consent',
-            },
-            redirectTo: `${hostingUrl}/login`
-        },
-    });
-    if (error) throw error
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      queryParams: {
+        access_type: 'offline',
+        prompt: 'consent'
+      },
+      redirectTo: `${HOSTING_URL}/login`
+    }
+  });
+  if (error) throw error;
 
-    return data
-}
+  return data;
+};
 
 export const googleSignOut = async () => {
-    const { error } = await supabase.auth.signOut();
-    if (error) throw error
-}
+  const { error } = await supabase.auth.signOut();
+  if (error) throw error;
+};
 
 export const getUserAuth = async () => {
-    const { data, error } = await supabase.auth.getUser();
-    if (error) throw error
-    return data
-}
+  const { data, error } = await supabase.auth.getUser();
+  if (error) throw error;
+  return data;
+};
 
 export const getUserDataFromSession = async () => {
-    try {
-        const data = await getUserAuth();
-        const { data: userData } = await supabase
-            .from('users')
-            .select('*')
-            .eq('email', data.user.email)
-            .single();
+  try {
+    const data = await getUserAuth();
+    const { data: userData } = await supabase.from('users').select('*').eq('email', data.user.email).single();
 
-        if (userData) {
-            const { id, email, name, profile_picture } = userData;
+    if (userData) {
+      const { id, email, name, profile_picture } = userData;
 
-            return { userId: id, email, name, profileImg: profile_picture }
-        }
-
-    } catch (error) {
-        return null;
+      return { userId: id, email, name, profileImg: profile_picture };
     }
-}
+  } catch (error) {
+    return null;
+  }
+};
